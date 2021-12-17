e1fe3de7366b9c2fd1ecfd864198660b7d0a3c48
Dzmitry Lyshko <lyshko.dmitry@gmail.com>
2021-12-17 14:28:43 +0200

    add framework files

---
+++ config/instance.js
@@ -0,0 +1,12 @@
+const instance = {
+  numberOfInstances: 4,
+  instanceSeries: "N1",
+  instanceMachineType: "n1-standard-8",
+  gpuType: "NVIDIA Tesla V100",
+  numberOfGPUs: "1",
+  localSSD: "2x375 GB",
+  datacenterLocation: "Frankfurt (europe-west3)",
+  commitedUsage: "1 Year",
+};
+module.exports = instance;
+
---
+++ test/specs/test4.js
@@ -0,0 +1,60 @@
+expect = require('chai').expect;
+
+const PricingCalculatorPage = require('../../lib/pricing_calculator_page');
+const pricingCalculatorPage = new PricingCalculatorPage();
+const YopmailPage = require('../../lib/yopmail_page');
+const yopmailPage = new YopmailPage();
+
+describe('pricing calculator page scenarios', function () {
+  searchQuery = 'Google Cloud Platform Pricing Calculator';
+  savedRecievedBill = '';
+  savedActualEstimatedCost = '';
+
+  before(async function () {
+    await pricingCalculatorPage.open();
+    await pricingCalculatorPage.maximizeWindow();
+    await pricingCalculatorPage.enterSearchQuery();
+    await pricingCalculatorPage.choosePricingCalculator();
+    await pricingCalculatorPage.confirmCookies();
+    await pricingCalculatorPage.switchToIFrame();
+
+    await pricingCalculatorPage.enterNumberOfInstances();
+    await pricingCalculatorPage.selectInstanceSeries();
+    await pricingCalculatorPage.selectInstanceMachineType();
+    await pricingCalculatorPage.scrollTo(
+      pricingCalculatorPage.addToEstimateBtn
+    );
+    await pricingCalculatorPage.selectCheckboxAddGPUs();
+    await pricingCalculatorPage.selectGPUTypeDropdown();
+    await pricingCalculatorPage.selectNumberOfGPUs();
+    await pricingCalculatorPage.selectLocalSSD();
+    await pricingCalculatorPage.selectDatacenterLocation();
+    await pricingCalculatorPage.selectCommitedUsage();
+    await pricingCalculatorPage.clickAddToEstimateBtn();
+    savedActualEstimatedCost =
+      await pricingCalculatorPage.getActualEstimatedCost();
+    await pricingCalculatorPage.switchToNewTab();
+    await yopmailPage.acceptCookies();
+    await yopmailPage.generateRandomEmail();
+    const emailAdress = await yopmailPage.getGeneratedEmail();
+    await browser.switchWindow('Google Cloud Pricing Calculator');
+    await pricingCalculatorPage.switchToIFrame();
+    await pricingCalculatorPage.clickEmailEstimateBtn();
+    await pricingCalculatorPage.scrollTo(pricingCalculatorPage.sendEmailBtn);
+    await pricingCalculatorPage.enterEmailAdress(emailAdress);
+    await pricingCalculatorPage.sendEmail();
+    await browser.switchWindow('https://yopmail.com/ru/email-generator');
+    await browser.pause(3000);
+    await yopmailPage.checkEmail();
+    await yopmailPage.switchToMailIFrame();
+    savedRecievedBill = await yopmailPage.getRecievedBill();
+  });
+
+  it('Verify that Total Estimated Monthly Cost in the letter equals to the cost in the calculator', async function () {
+    expect(savedActualEstimatedCost).to.equal(savedRecievedBill);
+  });
+
+  after(async function () {
+    await pricingCalculatorPage.close();
+  });
+});
